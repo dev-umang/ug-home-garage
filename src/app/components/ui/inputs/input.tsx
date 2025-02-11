359e9a7b23a0d75cf567eda324f58e0a960b4481
Umang Joshi <dev.umangjoshi@gmail.com>
2025-02-11 13:19:23 +0530

    Input related chagnes

--- src/app/components/ui/inputs/input.tsx
+++ src/app/components/ui/inputs/input.tsx
@@ -3,10 +3,11 @@ import { FC, InputHTMLAttributes } from "react";
 type Props = InputHTMLAttributes<HTMLInputElement>;
 
 const Input: FC<Props> = (p) => (
-  <div className="relative">
+  <div className="relative inline-flex flex-col">
+    <label className="text-xs font-sans font-medium text-slate-600/60 pl-2">Label</label>
     <input
       {...p}
-      className={`${p.className ?? ""} border border-slate-500/50 rounded-sm p-2 focus:ring-1 ring-sky-600 outline-0`}
+      className={`${p.className ?? ""} border border-slate-500/50 rounded-sm  px-2 py-1 focus:ring-1 ring-sky-600 outline-0`}
     />
   </div>
 );
